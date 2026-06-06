32f087c74d056bc2e43fe3e6957d0f19bf390261
spydplayz <spydplayz@gmail.com>
2026-06-06 12:57:16 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='medicalChatBot',
+    version='0.1.0',
+    author="Alay",
+    description='A medical chatbot application',
+    packages=find_packages(),
+    install_requires=[]
+)
